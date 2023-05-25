83db8c946701bac2f56f375a090aa54fa759e9f5
renan580 <97376031+renan580@users.noreply.github.com>
2023-05-25 20:51:29 -0300

    limitação da área de arraste

--- js/dragndrop.js
+++ js/dragndrop.js
@@ -1,39 +1,58 @@
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
+    /* Move o div pelo header */
     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
   } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    /* sem header, move o div por qualquer parte do elemento:*/
     elmnt.onmousedown = dragMouseDown;
   }
 
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
-    // get the mouse cursor position at startup:
+    // Posição do mouse ao clicar:
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
 
   function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
-    // calculate the new cursor position:
+    // Calcula a nova posição do mouse:
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    // nova posição do elemento div:
+    console.log(elmnt.offsetWidth);
+    var positiontop = elmnt.offsetTop - pos2;
+    var positionleft = elmnt.offsetLeft - pos1;
+    var boxwidth = elmnt.offsetWidth;
+    var boxheight = elmnt.offsetHeight;
+    if ( positiontop >= 47 && positiontop <= (window.innerHeight - boxheight -5) ) {
+      elmnt.style.top = positiontop + "px";
+    }
+    if ( positiontop < 47 ) {
+      elmnt.style.top = "47px";
+    }
+    if ( positiontop > (window.innerHeight - boxheight - 5) ) {
+      elmnt.style.top = (window.innerHeight - boxheight - 5) + "px";
+    }
+    if ( positionleft >= 5 && positionleft <= (window.innerWidth - boxwidth -5) ) {
+      elmnt.style.left = positionleft + "px";
+    }
+    if ( positionleft < 5 ) {
+        elmnt.style.left = "5px";
+    }
+    if ( positionleft > (window.innerWidth - boxwidth - 5) ) {
+        elmnt.style.left = (window.innerWidth - boxwidth - 5) + "px";
+    }
   }
 
   function closeDragElement() {
-    /* stop moving when mouse button is released:*/
     document.onmouseup = null;
     document.onmousemove = null;
   }
